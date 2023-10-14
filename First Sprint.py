7618d0424418b46feccab2e7c64733b0ffbd0cd1
Ayham Fteita <ayhamftaita@gmail.com>
2023-10-14 17:23:13 -0500

    rooms CRUD commit

--- First Sprint.py
+++ First Sprint.py
@@ -82,5 +82,65 @@ def delete_floor(floor_id):
     conn.close()
     return jsonify({"message": "Floor deleted!"}), 200
 
+# Route for getting all rooms
+@app.route('/api/rooms', methods=['GET'])
+def get_all_rooms():
+    connection = connect_to_database()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM room")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(result)
+
+# Route for adding a new room
+@app.route('/api/rooms', methods=['POST'])
+def add_room():
+    data = request.json
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO room (capacity, number, floor_id) VALUES (%s, %s, %s)"
+    cursor.execute(insert_query, (data["capacity"], data["number"], data["floor_id"]))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Room added!"}), 201
+
+# Route for updating a room
+@app.route('/api/rooms/<int:room_id>', methods=['PUT'])
+def update_room(room_id):
+    data = request.json
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    update_query = "UPDATE room SET capacity=%s, number=%s, floor_id=%s WHERE id=%s"
+    cursor.execute(update_query, (data["capacity"], data["number"], data["floor_id"], room_id))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Room updated!"})
+
+# Route for deleting a room
+@app.route('/api/rooms/<int:room_id>', methods=['DELETE'])
+def delete_room(room_id):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    delete_query = "DELETE FROM room WHERE id = %s"
+    cursor.execute(delete_query, (room_id,))
+
+    if cursor.rowcount == 0:
+        cursor.close()
+        conn.close()
+        return jsonify({"error": "No Room found"}), 404
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Room deleted!"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
