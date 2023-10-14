b316989449b3c68ac6c6dedc4cc4a2d270d272ee
Ayham Fteita <ayhamftaita@gmail.com>
2023-10-14 17:16:26 -0500

    floors CRUD ops commit

--- First Sprint.py
+++ First Sprint.py
@@ -18,5 +18,69 @@ def connect_to_database():
 def index():
     return "<h1>Senior Citizen Facility - Room Occupancy System</h1>"
 
+# Route for getting all floors
+@app.route('/api/floors', methods=['GET'])
+def get_all_floors():
+    connection = connect_to_database()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM floor")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    if result:
+        return jsonify({"message": "All floors retrieved successfully.",
+                         "floors": result})
+    else:
+        return jsonify({"message": "No floors found."})
+
+# Route for adding a new floor
+@app.route('/api/floors', methods=['POST'])
+def add_floor():
+    data = request.json
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO floor (level, name) VALUES (%s, %s)"
+    cursor.execute(insert_query, (data["level"], data["name"]))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Floor added!"}), 201
+
+# Route for updating a floor
+@app.route('/api/floors/<int:floor_id>', methods=['PUT'])
+def update_floor(floor_id):
+    data = request.json
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    update_query = "UPDATE floor SET level=%s, name=%s WHERE id=%s"
+    cursor.execute(update_query, (data["level"], data["name"], floor_id))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Floor updated!"})
+
+# Route for deleting a floor
+@app.route('/api/floors/<int:floor_id>', methods=['DELETE'])
+def delete_floor(floor_id):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+
+    delete_query = "DELETE FROM floor WHERE id = %s"
+    cursor.execute(delete_query, (floor_id,))
+
+    if cursor.rowcount == 0:
+        cursor.close()
+        conn.close()
+        return jsonify({"error": "No Floor found"}), 404
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"message": "Floor deleted!"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
