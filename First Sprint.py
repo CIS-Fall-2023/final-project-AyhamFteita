f27c51212604381abcc372a4b1ce3d3e711768e7
Ayham Fteita <ayhamftaita@gmail.com>
2023-10-14 15:45:41 -0500

    Initial Flask App Setup

--- First Sprint.py
+++ First Sprint.py
@@ -1,1 +1,22 @@
-#Final Project - First Sprint 
+import mysql.connector
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Database connection function
+def connect_to_database():
+    connection = mysql.connector.connect(
+        host="cis3368fall.c6xk1vtqaxor.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="Memo1999",
+        database="cis3368finalproject"
+    )
+    return connection
+
+# Route for the home page
+@app.route('/')
+def index():
+    return "<h1>Senior Citizen Facility - Room Occupancy System</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
